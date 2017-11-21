202ef9e951d5c7ab630de786c393e5fa4b23120b
Ashvin Warrier <warrierar@gmail.com>
2017-11-21 14:02:51 -0500

    Added skeletal User class with basic information

---
+++ User.py
@@ -0,0 +1,8 @@
+class User:
+    
+    def __init__(self, first_name, last_name, DOB, nick_name = None, PIN = None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.DOB = DOB
+        self.nick_name = nick_name
+        self.PIN = PIN
