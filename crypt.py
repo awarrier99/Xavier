87e3471449717ba57acb6fbd02aeff3e1f36cb97
Ashvin Warrier <warrierar@gmail.com>
2017-11-27 06:19:14 -0500

    Keeping skeletal files in case of future use

--- crypt.py
+++ crypt.py
@@ -1,7 +1,10 @@
+from Crypto import crypt, convert
 
-def encrypt(m):
-    return m
 
+def encrypt(x):
+    m, pad = convert.t_to_a(x)
+    print(x, m, "\n\n")
+    c, d, n = crypt.encrypt(m)
+    print("\n", c, "\n")
 
 def decrypt(m):
-    return m
