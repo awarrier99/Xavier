dac1da876bc1920462ace4fac286cf3b4e214ba8
Ashvin Warrier <warrierar@gmail.com>
2017-12-21 17:14:30 -0500

    Created constructor for user class

--- user/user.go
+++ user/user.go
@@ -5,11 +5,15 @@ import (
     "time"
 )
 
-type User struct {
+type user struct {
     first_name string
     last_name string
     nick_name string
     age int
     DOB Time
     PIN int
+}
+
+func NewUser(first_name, last_name, nick_name string, age int, DOB Time, PIN int) *user {
+    return &user{first_name, last_name, nick_name, age, DOB, PIN}
 }
